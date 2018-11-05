879bfdf1d57a592dab9094c4c9e800c1d6a6c9d3
Jeff Tinker <jtinker@google.com>
2018-11-05 15:04:17 -0800

    oxygen: Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug:117570956
    bug:110838441
    bug:116252891
    Change-Id: Ie9373543ef416e718a70e75d95bf5b15a2852705

--- msm8953.mk
+++ msm8953.mk
@@ -168,7 +168,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # Ebtables
 PRODUCT_PACKAGES += \
