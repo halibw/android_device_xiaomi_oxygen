a9a2ab06bd0683071293178d48171c5fdab71f6e
Artem Borisov <dedsa2002@gmail.com>
2018-11-07 12:25:37 +0400

    oxygen: Enable USB via ConfigFS
    Required on 4.9 now.
    
    Change-Id: I1b4557da497c3c63d64770f94a2ff56e72602f8a

--- device.mk
+++ device.mk
@@ -345,6 +345,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.goodix.sh \
