990e11b3dc750ba4bedeb307e8527421c0ee6fe3
Ayaanmalikbrt <43542602+Ayaanmalikbrt@users.noreply.github.com>
2018-11-10 22:36:38 +0530

    msm8953-common: Use qti bluetooth and ant+ stack

--- msm8953.mk
+++ msm8953.mk
@@ -114,6 +114,11 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# ANT
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
