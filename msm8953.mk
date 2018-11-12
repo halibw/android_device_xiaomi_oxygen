f713c2c9043cea7463dad50e2b17a468cffc7ff3
Rakesh Roshan <rakeshraimca@gmail.com>
2018-11-12 10:21:00 +0530

    adding back some configs

--- msm8953.mk
+++ msm8953.mk
@@ -123,9 +123,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
+    camera.msm8953 \
+    libmm-qcamera \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Charging
 PRODUCT_PACKAGES += \
