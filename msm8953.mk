a8483a62382301990b7a7158cf4f2ff1014bc2a3
Rakesh Roshan <rakeshraimca@gmail.com>
2018-12-01 01:15:18 +0530

    adding health hal 2.0

--- msm8953.mk
+++ msm8953.mk
@@ -212,6 +212,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0 \
+
 # Healthd
 PRODUCT_PACKAGES += \
     chargeonlymode
