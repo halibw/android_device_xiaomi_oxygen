fa0379cf86e0c0fd43a9dee1935e052f279dcdeb
Rakesh Roshan <rakeshraimca@gmail.com>
2018-12-02 23:32:18 +0530

    not sure why i did this

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -62,24 +62,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
   vidc.enc.dcvs.extra-buff-count=2 \
   persist.camera.HAL3.enabled=1
 
-#gaming
--PRODUCT_PROPERTY_OVERRIDES += \
-  debug.composition.type=gpu \
-  debug.egl.hw=1 \
-  debug.egl.profiler=1 \
-  debug.enabletr=true \
-  debug.gralloc.enable_fb_ubwc=1 \
-  debug.overlayui.enable=1 \
-  debug.performance.tuning=1 \
-  debug.sf.hw=1 \
-  dev.pm.dyn_samplingrate=1 \
-  hw3d.force=1 \
-  hwui.disable_vsync=true \
-  hwui.render_dirty_regions=false \
-  ro.config.enable.hw_accel=true \
-  ro.fb.mode=1 \
-  ro.product.gpu.driver=1 \
-  ro.sf.compbypass.enable=0
 
 #disable UBWC for camera
 PRODUCT_PROPERTY_OVERRIDES += \
