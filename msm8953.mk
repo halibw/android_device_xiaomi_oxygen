1f672fd1126cfd0f788ae4e3b8bdc70616462254
Rakesh Roshan <rakeshraimca@gmail.com>
2018-12-22 17:29:14 +0530

    msm8953:-common: Add public.libraries.txt

--- msm8953.mk
+++ msm8953.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
     MGC \
 
 
+# Public Libraries
+ PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Charging
 PRODUCT_PACKAGES += \
     charger_res_images
