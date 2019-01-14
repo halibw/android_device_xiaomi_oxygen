d5655584614838ef7832135f11941ba57beb753e
Rakesh Roshan <rakeshraimca@gmail.com>
2019-01-14 17:45:43 +0530

    some cleanup and added Perfect snap camera

--- msm8953.mk
+++ msm8953.mk
@@ -134,13 +134,11 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Gcam \
     Snap \
+    Gcam \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
-PRODUCT_PACKAGES += \
-    MGC \
 
 # Camera
 #PRODUCT_PACKAGES += \
