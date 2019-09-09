3efc1c9e7255f7dd816ffe50af0e7aebe5a93e6b
aka-scissor <akascissor9@gmail.com>
2019-09-09 18:55:27 +0000

    oxygen: remove lineage stuff

--- msm8953.mk
+++ msm8953.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+#DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 #PRODUCT_ENFORCE_RRO_TARGETS := \
     Bluetooth \
