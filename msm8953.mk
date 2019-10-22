4786709b08857c2571cece9eec04b9e9983884fe
AyaanSaleemMalik <scissordragonboy1@gmail.com>
2019-10-22 17:05:09 +0000

    msm8953-common:configstore 1.0=>1.1

--- msm8953.mk
+++ msm8953.mk
@@ -149,7 +149,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service
+    android.hardware.configstore@1.1-service
 
 # Camera-face detection
 #PRODUCT_PACKAGES += \
