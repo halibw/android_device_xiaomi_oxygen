5fe12d205076de1db5411eba887ad90d8d235952
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    oxygen: Switch to Audio HAL V6
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- device.mk
+++ device.mk
@@ -82,10 +82,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
