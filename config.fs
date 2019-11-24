7619e44fc222d84ac464274bb4cc83d936f42c78
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-11-24 13:16:31 +0000

    wifi: test based changes

--- config.fs
+++ config.fs
@@ -64,6 +64,13 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+
+[vendor/bin/xtwifi-client]
+mode: 0755
+user: AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
