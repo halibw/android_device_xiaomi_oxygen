f35bff1c5b8303a2d4806eb886e2a04c7515c7ae
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-11-26 10:43:52 +0000

    Revert "add camera configs"
    This reverts commit b640a47a5f77a579af01ea9678c0adb368306aa1.

--- device.mk
+++ device.mk
@@ -122,20 +122,12 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor_32 \
     camera.msm8953 \
     libmm-qcamera \
     Gcam \
     Snap
-
-# camera 2
-PRODUCT_PACKAGES += \
-    Camera2
 
 # Consumerir
 PRODUCT_PACKAGES += \
