5d9b8eceffc8e88a64f45e260b6af4d261607deb
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-11-27 09:54:32 +0000

    oxygen: Build fingerprint wrapper

--- device.mk
+++ device.mk
@@ -170,7 +170,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_oxygen
+    fingerprintd \
+    fingerprint.msm8953
 
 # FM
 PRODUCT_PACKAGES += \
