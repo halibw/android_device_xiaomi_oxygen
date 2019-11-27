981906ca0c957152c905674381f1cfb64a6df411
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-11-27 10:05:14 +0000

    camera: dont replace stock camera with gcam

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -94,22 +94,6 @@ LOCAL_SRC_FILES    := init.qti.fm.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := post-fs-data.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/post-fs-data.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := service.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/service.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 # Offmode charging
 include $(CLEAR_VARS)
 LOCAL_MODULE          := chargeonlymode
