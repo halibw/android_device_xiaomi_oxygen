e76947032ca9405801e6c8d6c3aef00032b14265
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-11-27 10:01:00 +0000

    drop camera.msm8953 * build camera hals

--- device.mk
+++ device.mk
@@ -124,10 +124,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8953 \
+    camera.device@3.2-impl \
     libmm-qcamera \
     Gcam \
-    Snap
+    Snap \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Consumerir
 PRODUCT_PACKAGES += \
