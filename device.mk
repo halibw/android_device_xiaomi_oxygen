0346c46e3d6425f95419735c6ad333707a413e73
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-12-24 17:37:16 +0000

    oxygen: wifi-display: drop wfd bootjars

--- device.mk
+++ device.mk
@@ -417,5 +417,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
