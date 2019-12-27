b65155d2433530e795afe7f83992968b26fdb545
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-12-27 11:30:13 +0000

    oxygen: device.mk: build camera.msm8953 and snap
    * drop gcam build

--- device.mk
+++ device.mk
@@ -125,8 +125,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    camera.msm8953 \
     libmm-qcamera \
-    Gcam \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
