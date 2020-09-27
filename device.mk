1ceb6ac75a275eb012d82ff20a6231032c984f5e
Ryan Mitchell <rtmitchell@google.com>
2020-09-27 19:07:58 +0200

    oxygen: Use component-override to configure Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default.
    
    Remove usages of profile_supported_* from overlays as they no
    longer configure the enable state of Bluetooth components.
    
    Bug: 135048762
    Test: adb shell pm query-services \
          com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
          adb shell pm query-services \
          com.android.bluetooth/.hfpclient.HeadsetClientService
          adb shell pm query-services \
          com.android.bluetooth/.sap.SapService

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
     libui_shim \
     libcamshim
 
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Consumerir
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service.xiaomi_oxygen
