d65cea84479d07bbba6406f70a4bdda225458b39
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-23 16:39:35 +0100

    oxygen: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?

--- device.mk
+++ device.mk
@@ -84,7 +84,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl:32 \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     audio.primary.msm8953:32 \
