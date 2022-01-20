8abecf13a5ed0ffd23b28cca6519b7a7bed4f06d
Arvind Kumar <karvin@codeaurora.org>
2022-01-20 22:04:26 +0100

    oxygen: Add vndservicemanager explicitly
    This is now added to product_packages based on shipping_api which is
    not yet supported on few targets. Hence adding it explicitly

--- device.mk
+++ device.mk
@@ -392,6 +392,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# vndservicemanager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
