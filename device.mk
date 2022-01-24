e58922db926b1da1dddb36144b2f03603e4673ea
halibw <bymark256@gmail.com>
2022-01-24 20:37:04 +0800

    oxygen: device: Build 64-bit impl

--- device.mk
+++ device.mk
@@ -355,7 +355,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
@@ -368,7 +368,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-impl:64 \
     android.hardware.thermal@1.0-service \
     thermal.msm8953
 
