a5eb4d317e90d389e7527d29b67c0cd7885d5696
halibw <bymark256@gmail.com>
2022-01-25 02:48:51 +0800

    oxygen: device: Build android.frameworks.displayservice@1.0 commit https://github.com/zeelog/android_device_xiaomi_mido/commit/5670bef3aca21a5f48d58dc9be24bf38c16ddeaf

--- device.mk
+++ device.mk
@@ -124,6 +124,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+   android.frameworks.displayservice@1.0_32 \
    android.hardware.camera.device@3.5 \
    android.hardware.camera.common@1.0 \
    android.hardware.camera.provider@2.4-impl \
