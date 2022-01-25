da3ca067d670afac1acbb6e05cfdb4e873b150ec
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    oxygen: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device.mk
+++ device.mk
@@ -91,7 +91,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl:32 \
+    android.hardware.bluetooth.audio-impl \
     audio.primary.msm8953:32 \
     audio.bluetooth.default \
     audio.r_submix.default \
