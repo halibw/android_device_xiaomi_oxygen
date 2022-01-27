303f57a7396277c4e68233917339165dcc32a2e6
halibw <bymark256@gmail.com>
2022-01-27 15:43:54 +0800

    oxygen: camera: Build camera shims Change-Id: a17676290ed612d2e564d456555920e55a91f1bb

--- device.mk
+++ device.mk
@@ -126,25 +126,25 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-   android.frameworks.displayservice@1.0_32 \
-   android.hardware.camera.device@3.5 \
-   android.hardware.camera.common@1.0 \
-   android.hardware.camera.provider@2.4-impl \
-   android.hardware.camera.provider@2.4-service \
-   vendor.qti.hardware.camera.device@1.0 \
-   vendor.qti.hardware.camera.device@1.0_vendor \
-   camera.device@3.2-impl \
-   libqomx_core \
-   camera.msm8953 \
-   libcamshim \
-   libmm-qcamera \
-   libui_shim \
-   Snap
+    android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    camera.device@3.2-impl \
+    libqomx_core \
+    camera.msm8953 \
+    libmm-qcamera \
+    Snap
 
 # Shims
 PRODUCT_PACKAGES += \
     libbinder_gdxfp \
-    fakelogprint
+    fakelogprint \
+    libui_shim \
+    libcamshim
 
 # Consumerir
 PRODUCT_PACKAGES += \
