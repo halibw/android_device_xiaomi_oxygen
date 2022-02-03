ec851ffccb2ac249bc8cefcb499f4493275130c4
halibw <bymark256@gmail.com>
2022-02-03 19:17:52 +0800

    oxygen: device: Build libqti_vndfwk_detect * fix: CANNOT LINK EXECUTABLE "/vendor/bin/cnd": library "libqti_vndfwk_detect.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -315,7 +315,11 @@ PRODUCT_COPY_FILES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
