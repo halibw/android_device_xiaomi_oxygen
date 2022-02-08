4c4a34cec0e390880657f67c460ced5bd4f6afe6
halibw <bymark256@gmail.com>
2022-02-08 18:17:21 +0800

    oxygen: Telephony: Disable advance network scan for legacy targets - Set persist.vendor.radio.enableadvancedscan to false
    Change-Id: Id01e7a545e89ff9ab9fb659aec1fbae527c40f71

--- prop.mk
+++ prop.mk
@@ -124,6 +124,7 @@ persist.radio.aosp_usr_pref_sel=true \
 persist.radio.multisim.config=dsds \
 persist.vendor.radio.add_power_save=1 \
 persist.vendor.radio.custom_ecc=1 \
+persist.vendor.radio.enableadvancedscan=false \
 persist.vendor.radio.procedure_bytes=SKIP \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
