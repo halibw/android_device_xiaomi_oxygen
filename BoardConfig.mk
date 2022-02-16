135696c5713e9a12de436810d47c1209d53db616
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:38:45 -0500

    oxygen: specify androidboot.boot_devices
    so that /dev/block/by-name/ symlinks are created.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I6d12092e9326bbc8097a5b40a58ba0286a406523

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ BOARD_BOOT_HEADER_VERSION := 1
 TARGET_KERNEL_CONFIG := oxygen_defconfig
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 loop.max_part=16 androidboot.usbconfigfs=true
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/7824900.sdhci
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
