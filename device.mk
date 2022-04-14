f308030f3d3fc45c1ef7a2dd3308ce2125b3b07e
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 23:46:09 -0700

    oxygen: Drop all component overrides
     * Qualcomm no longer enables these for targets
       that support RRO, e.g. all QSSI targets.
    
    Change-Id: I0372817623e8d0e58926fbfb519671c3caa07b86

--- device.mk
+++ device.mk
@@ -164,10 +164,6 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Consumerir
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
