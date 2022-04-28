db9c35202a02ff79bee8bb3134bb12d29636885a
halibw <bymark256@gmail.com>
2022-04-28 19:40:17 +0800

    oxygen: camera: Restore build camera oss * Delete features camera frameworks enhancements

--- device.mk
+++ device.mk
@@ -134,11 +134,19 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.device@3.5 \
     android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.3:64 \
+    android.hardware.camera.device@3.4:64 \
+    android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.provider@2.4 \
+    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     libqomx_core \
+    camera.msm8953 \
     libmm-qcamera \
     libui_shim \
     libshims_camera \
