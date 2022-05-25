c8dc80909c00a892332b15fa0179b7b920414314
halibw <bymark256@gmail.com>
2022-05-25 14:33:14 +0800

    oxygen: initial fp keylayout
    * Disable all events related to fingerprint

--- device.mk
+++ device.mk
@@ -247,6 +247,11 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
