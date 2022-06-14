a18d908cb5b342c8137ec423ae7df52888278fc3
halibw <bymark256@gmail.com>
2022-06-14 18:16:09 +0800

    oxygen: overlay: Add lineage-sdk overlay
    * Move config_cleanupUnusedFingerprints to the new location
    * Move config_globallyDisabledComponents to the new location

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
