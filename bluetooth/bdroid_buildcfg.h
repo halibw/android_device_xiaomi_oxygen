2bf98b34361039a011c0fe33a6ebfecde5318e7e
halibw <bymark256@gmail.com>
2022-07-09 22:11:57 +0800

    oxygen: bluetooth: Set default device name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,6 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 // Disables read remote device feature
+#define BTM_DEF_LOCAL_NAME "Mi Max 2"
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
