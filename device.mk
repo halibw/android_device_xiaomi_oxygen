a3733c8d0b3e97f15d45a9302749d4ea7b0a7e58
halibw <bymark256@gmail.com>
2022-07-19 21:44:23 +0800

    oxygen: fstab: Switch to FBE

--- device.mk
+++ device.mk
@@ -251,10 +251,6 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
