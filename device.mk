70410f6439ba721e3a254d11f0bf88bce8dced84
halibw <bymark256@gmail.com>
2022-08-07 01:55:02 +0800

    oxygen: Drop fake thermal.msm8953 target
    The definition for this module was not found in the source code.
    
    Change-Id: Idb68509aa60fb22e1af4a48c29504b0bf397636c

--- device.mk
+++ device.mk
@@ -398,8 +398,7 @@ PRODUCT_COPY_FILES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8953
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
