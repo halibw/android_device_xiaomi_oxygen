09c15b867194d485b3dd82d16d32428e5611a13f
halibw <bymark256@gmail.com>
2022-08-14 16:58:08 +0800

    oxygen: Drop duplicate camera HALs
    CameraProvider already includes these libs, no need to define them again.
    
    Change-Id: I3a6415a7bdac3106484feb052a52f2ce634127d5

--- device.mk
+++ device.mk
@@ -144,11 +144,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.device@3.3 \
-    android.hardware.camera.device@3.4 \
-    android.hardware.camera.device@3.5 \
-    android.hardware.camera.provider@2.4 \
-    android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0 \
     camera.msm8953 \
