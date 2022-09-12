113377e41612541742abd5755a6bee5ce38eaab3
halibw <bymark256@gmail.com>
2022-09-13 01:58:13 +0800

    oxygen: Restore vendor.qti.hardware.perf build
    Decoder still needs it.
    
    Change-Id: I6236894ae49c0f9a9110e84714cb7ae7afa7d331

--- device.mk
+++ device.mk
@@ -311,7 +311,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-msm8953-libperfmgr \
     android.hardware.power@1.2.vendor \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
