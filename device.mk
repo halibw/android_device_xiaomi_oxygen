e1882f4a7b5f2b7a855541872d5c03361d57a015
halibw <bymark256@gmail.com>
2022-09-12 19:16:50 +0800

    oxygen: libperfmgr: Rename for xiaomi-msm8953
    Change-Id: I2d1643b3856ff376d563a69044335c2522307c15

--- device.mk
+++ device.mk
@@ -315,6 +315,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service.xiaomi-msm8953-libperfmgr \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
