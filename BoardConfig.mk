a3e02c31c594c6a4126a3ef02faaf99aa43d2cea
halibw <bymark256@gmail.com>
2022-10-01 23:15:34 +0800

    oxygen: sepolicy: Address linkerconfig denials
    Change-Id: I450693fd1d8a385ed60f9a0de5e5f7e59dcb148a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,6 +142,7 @@ VENDOR_SECURITY_PATCH := 2022-06-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
