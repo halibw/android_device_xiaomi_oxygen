0ff24d87906cda58c0f541e250097a9aeb32e5d2
Jimmy Shiu <jimmyshiu@google.com>
2022-10-19 01:13:08 +0800

    oxygen: ADPF: fix use-after-free crash
    The main problem is the timer thread could be woken after the session
    was destroyed. We did have a closed flag which was set in destructor and the flag would be checked before handleMessage accessing the session
    instance. To fix the problem, the operations of flag checking and session instance accessing should be guarded by the lock.
    
    Bug: 236674672
    Test: manual test
    Change-Id: I49a18efbc135b1bc070b101038a8a0bcc6e19fec

--- power-libperfmgr/aidl/PowerHintSession.cpp
+++ power-libperfmgr/aidl/PowerHintSession.cpp
@@ -265,14 +265,10 @@ ndk::ScopedAStatus PowerHintSession::close() {
     }
     // Remove the session from PowerSessionManager first to avoid racing.
     PowerSessionManager::getInstance()->removePowerSession(this);
-    setSessionUclampMin(0);
-    {
-        std::lock_guard<std::mutex> guard(mSessionLock);
-        mSessionClosed.store(true);
-    }
-    mDescriptor->is_active.store(false);
     mEarlyBoostHandler->setSessionDead();
     mStaleTimerHandler->setSessionDead();
+    setSessionUclampMin(0);
+    mDescriptor->is_active.store(false);
     updateUniveralBoostMode();
     return ndk::ScopedAStatus::ok();
 }
@@ -505,6 +501,7 @@ void PowerHintSession::StaleTimerHandler::updateTimer(time_point<steady_clock> s
 }
 
 void PowerHintSession::StaleTimerHandler::handleMessage(const Message &) {
+    std::lock_guard<std::mutex> guard(mClosedLock);
     if (mIsSessionDead) {
         return;
     }
@@ -534,7 +531,7 @@ void PowerHintSession::StaleTimerHandler::handleMessage(const Message &) {
 }
 
 void PowerHintSession::StaleTimerHandler::setSessionDead() {
-    std::lock_guard<std::mutex> guard(mStaleLock);
+    std::lock_guard<std::mutex> guard(mClosedLock);
     mIsSessionDead = true;
     PowerHintMonitor::getInstance()->getLooper()->removeMessages(mSession->mStaleTimerHandler);
 }
