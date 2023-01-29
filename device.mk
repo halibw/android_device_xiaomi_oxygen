b754c48abbfb3f116127a488b36e2eaab0e38602
Haris Wu <bymark256@gmail.com>
2023-01-29 16:28:04 +0800

    Revert "oxygen: Add vndservicemanager explicitly"
    This reverts commit 8abecf13a5ed0ffd23b28cca6519b7a7bed4f06d.
    
    No targets were found to call context manager binder services.
    
    Change-Id: Ia7ab7a8340e3a6a4651b8d236ecb144199aeefeb

--- device.mk
+++ device.mk
@@ -377,10 +377,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
-# vndservicemanager
-PRODUCT_PACKAGES += \
-    vndservicemanager
-
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
